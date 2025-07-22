53a1e16668b011a09163459066f8dda9d9be805f
SHIELD SYSTEM <SHIELD SYSTEM@ASUS-PC>
2025-07-22 05:57:06 +0330

    Grid

--- MauiApp1/App.xaml.cs
+++ MauiApp1/App.xaml.cs
@@ -9,7 +9,7 @@
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new TabbedPageDemo());
+            return new Window(new GridDemoPage());
         }
     }
 }
---
+++ MauiApp1/GridDemoPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiApp1;
+
+public partial class GridDemoPage : ContentPage
+{
+	public GridDemoPage()
+	{
+		InitializeComponent();
+	}
+}
